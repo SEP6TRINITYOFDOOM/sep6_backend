9090ca8168b60c948c6565d5c242268feff22a31
adamarasimowicz <a.arasimowicz@gamedia.pl>
2023-10-26 10:57:37 +0200

    init commit

---
+++ src/main/java/com/sep6/app/Sep6BackendApplication.java
@@ -0,0 +1,13 @@
+package com.sep6.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep6BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sep6BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sep6/app/Sep6BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep6.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep6BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
