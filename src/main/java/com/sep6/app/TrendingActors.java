afa295ca36c4e7fb484b38d1b6519e9df0bcd417
Esben Fogh <esbenvensel.fogh@gmail.com>
2023-12-06 15:30:58 +0100

    Lombok creates errors where as traditional Getters and Setters works

--- src/main/java/com/sep6/app/TrendingActors.java
+++ src/main/java/com/sep6/app/TrendingActors.java
@@ -1,10 +1,5 @@
 package com.sep6.app;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class TrendingActors {
     public TrendingActors(){
 
@@ -15,4 +10,35 @@ public class TrendingActors {
     int total_pages;
     int total_results;
 
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public Actor[] getResults() {
+        return results;
+    }
+
+    public void setResults(Actor[] results) {
+        this.results = results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
 }
--- src/main/java/com/sep6/app/TrendingMovies.java
+++ src/main/java/com/sep6/app/TrendingMovies.java
@@ -1,10 +1,5 @@
 package com.sep6.app;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
 public class TrendingMovies {
 
     public TrendingMovies(){
@@ -16,4 +11,35 @@ public class TrendingMovies {
     int total_pages;
     int total_results;
 
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public Movie[] getResults() {
+        return results;
+    }
+
+    public void setResults(Movie[] results) {
+        this.results = results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
 }
